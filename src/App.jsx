81b1920d6e091774cf0787e1411ee373ce99f164
Namoun media <namounmediadev@gmail.com>
2025-12-16 22:34:20 +0100

    F01: App > Create page login

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstname, setFirstname] = useState("");
 
+  const handleClick = () => {
+    if (firstname === "") {
+       alert(`Veuillez remplir ce champ`);
+    }
+     alert(`Bonjour ${firstname}`);
+      setFirstname("");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite Amadou Dioubaté</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Bienvenue chez nous !</h2>
+      <form>
+        <h4>Connectez vous</h4>
+        <input type="text" 
+          placeholder="Entrez votre prénom"
+          required
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <button onClick={handleClick}>Accédez à votre espace</button>
+      </form>
+    </div>
   )
 }
 
