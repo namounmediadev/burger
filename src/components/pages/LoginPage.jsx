cf099fb03f3a00cdbaedd24076739ca25023c50a
Namoun media <namounmediadev@gmail.com>
2025-12-17 00:06:18 +0100

    ✨F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 
 function Form() {
+  // State
   const [firstname, setFirstname] = useState("");
 
-  const handleClick = () => {
-    if (firstname === "") {
-      alert(`Veuillez remplir ce champ`);
-    }
+  // Set of behaviors
+  const handleSubmit = (e) => {
+    e.preventDefault();
     alert(`Bonjour ${firstname}`);
     setFirstname("");
   };
+
+  // Render
   return (
     <div>
-      <h2>Bienvenue chez nous !</h2>
-      <form>
-        <h4>Connectez vous</h4>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Entrez votre prénom"
+          placeholder="Entrez votre prénom..."
           required
           value={firstname}
           onChange={(e) => setFirstname(e.target.value)}
         />
-        <button onClick={handleClick}>Accédez à votre espace</button>
+        <button>Accédez à votre espace</button>
       </form>
     </div>
   );
