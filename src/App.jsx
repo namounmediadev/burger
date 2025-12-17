014be4b225ce1fba64d77b9a040c081ba8d5c4c5
Namoun media <namounmediadev@gmail.com>
2025-12-17 23:57:23 +0100

    🚚F02: 🗂️ move OrderPage and ErrorPage into dedicated folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './components/pages/login/LoginPage';
-import OrderPage from './components/pages/OrderPage';
-import ErrorPage from './components/pages/ErrorPage';
+import OrderPage from './components/pages/order/OrderPage';
+import ErrorPage from './components/pages/error/ErrorPage';
 
 
 
